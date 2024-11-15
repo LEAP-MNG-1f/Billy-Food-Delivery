b56a5a87270fa57be12ec788e0f381f88260eecb
billduluu <billduluu@gmail.com>
2024-11-15 10:43:47 +0800

    h

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
-import Image from "next/image";
 import { useEffect } from "react";
 import Alert from "@mui/material/Alert";
-import CheckIcon from "@mui/icons-material/Check";
 import AcUnitIcon from "@mui/icons-material/AcUnit";
 import AnchorTemporaryDrawer from "./_components/Drawer";
+import { Card } from "./_components/Card";
 
 export default function Home() {
   const fetchData = async () => {
@@ -19,16 +18,15 @@ export default function Home() {
   useEffect(() => {
     fetchData();
   }, []);
+  const price: number = 1000;
+  let str: string = "Hello";
+  let bool: boolean = true;
 
   return (
     <div>
-      <Alert
-        severity="success"
-        icon={<AcUnitIcon fontSize="inherit" className="  text-indigo-600" />}
-      >
-        Here is a gentle confirmation that your action was successful.
-      </Alert>
-      <AnchorTemporaryDrawer />
+      <Card img="Zurag" title="Burger" price={5500} discount={20} />
+      <Card img="Zurag" title="Burger" price={5500} />
+      {/* <AcUnitIcon /> */}
     </div>
   );
 }
